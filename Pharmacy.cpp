8682bf547d606aa16c5caf4ecf6297c4a1fef485
Tomas Homola <homola.tom6@gmail.com>
2020-12-19 22:51:46 +0100

    Done for today

--- Pharmacy.cpp
+++ Pharmacy.cpp
@@ -12,27 +12,210 @@ QString adminPassword = "adminpassword";
 
 */
 
+bool Pharmacy::loadUsers()
+{
+    QFile file("_usersData/admin.txt");
+    
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        qDebug() << "File not opened";
+        return false;
+    }
+    else
+    {
+        qDebug() << "File OPENED";
+        QTextStream fromFile(&file);
+
+        QString temp = fromFile.readLine();
+
+        admin.setPassword(temp);
+
+        file.close();
+        return true;
+    }
+}
+
+bool Pharmacy::saveUsers()
+{
+    // !file.open(QIODevice::ReadOnly | QIODevice::Text)
+    QFile adminFile("_usersData/admin.txt");
+    QFile customersFile("_usersData/customers.txt");
+    QFile premiumCustomersFile("_usersData/premiumCustomers.txt");
+    QFile employeesFile("_usersData/employees.txt");
+
+    if (!adminFile.open(QIODevice::WriteOnly | QIODevice::Text) && !customersFile.open(QIODevice::WriteOnly | QIODevice::Text) && !premiumCustomersFile.open(QIODevice::WriteOnly | QIODevice::Text) && !employeesFile.open(QIODevice::WriteOnly | QIODevice::Text))
+    {
+        qDebug() << "Files not opened";
+        return false;
+    }
+    else
+    {
+        QTextStream out1(&adminFile);
+
+        out1 << admin.getPassword();
+
+        QTextStream out2(&customersFile);
+        for (int i = 0; i < customers.size(); i++)
+        {
+            out2 << customers[i].getName() << "\n" << customers[i].getSurname() << "\n" << customers[i].getAdress() << "\n" << customers[i].getLogin() << "\n" << customers[i].getPassword();
+        }
+
+        QTextStream out3(&premiumCustomersFile);
+        for (int i = 0; i < premiumCustomers.size(); i++)
+        {
+            out3 << premiumCustomers[i].getName() << "\n" << premiumCustomers[i].getSurname() << "\n" << premiumCustomers[i].getAdress() << "\n" << premiumCustomers[i].getDiscount() << "\n" << premiumCustomers[i].getLogin() << "\n" << premiumCustomers[i].getPassword();
+        }
+
+        QTextStream out4(&employeesFile);
+        for (int i = 0; i < employees.size(); i++)
+        {
+            out4 << employees[i].getPosition() << "\n" << employees[i].getLogin() << "\n" << employees[i].getPassword();
+        }
+
+        adminFile.close();
+        customersFile.close();
+        premiumCustomersFile.close();
+        employeesFile.close();
+        
+        return true;
+    }
+}
+
 Pharmacy::Pharmacy(QWidget *parent) : QMainWindow(parent)
 {
     ui.setupUi(this);
 
-    ui.groupBox_Login->setVisible(false);
+    // groupBox_Main
+    ui.pushButton_SignOut->setVisible(false);
 
+    // groupBox_SignIn
+    ui.groupBox_SignIn->setVisible(false);
     ui.comboBox_users->addItem("admin");
 
+    // groupBox_AdminStuff
+    ui.groupBox_AdminStuff->setVisible(false);
+
+    loadUsers();
+
+}
+
+void Pharmacy::on_actionsave_users_triggered()
+{
+    saveUsers();
 }
 
 void Pharmacy::on_pushButton_SignInWindow_clicked()
 {
-    ui.groupBox_Login->setVisible(true);
+    // groupBox_Main
+    ui.label_SignedInAs->setText("Signed in as:");
+
+    // groupBox_SignIn
+    ui.groupBox_SignIn->setVisible(true);
+
+    // groupBox_AdminStuff
+    ui.groupBox_AdminStuff->setVisible(false);
+
+}
+
+void Pharmacy::on_pushButton_SignOut_clicked()
+{
+    // groupBox_Main
+    ui.pushButton_SignOut->setVisible(false);
+    ui.pushButton_SignInWindow->setVisible(true);
+    ui.label_SignedInAs->setText("Signed in as:");
+
+    //groupBox_AdminStuff
+    ui.groupBox_AdminStuff->setVisible(false);
+}
+
+void Pharmacy::on_pushButton_PrintUsers_clicked()
+{
+    qDebug() << admin.getLogin() << admin.getPassword();
+
+    for (int i = 0; i < customers.size(); i++)
+        qDebug() << customers[i].getName() << customers[i].getSurname() << customers[i].getAdress() << customers[i].getLogin() << customers[i].getPassword();
+
+    for (int i = 0; i < premiumCustomers.size(); i++)
+        qDebug() << premiumCustomers[i].getName() << premiumCustomers[i].getSurname() << premiumCustomers[i].getAdress() << premiumCustomers[i].getDiscount() << premiumCustomers[i].getLogin() << premiumCustomers[i].getPassword();
+
+    for (int i = 0; i < employees.size(); i++)
+        qDebug() << employees[i].getPosition() << employees[i].getLogin() << employees[i].getPassword();
 }
 
 void Pharmacy::on_pushButton_SignInConfirm_clicked()
 {
-    qDebug() << "Login:" << ui.comboBox_users->currentText();
-    qDebug() << "Password:" << ui.lineEdit_Password->text();
+    QString password = ui.lineEdit_Password->text(); // temporary string pre ulozenie zadaneho hesla
 
-    qDebug() << QCryptographicHash::hash(ui.lineEdit_Password->text().toStdString().c_str(), QCryptographicHash::Sha1).toHex();
+    ui.lineEdit_Password->setText(""); // vymazanie zadaneho hesla z lineEditu
+    
+    // premena zadaneho hesla na zasifrovanu formu cez nejaky hash, aby sa dalo porovnat s ulozenym heslom
+    password = QCryptographicHash::hash(password.toStdString().c_str(), QCryptographicHash::Sha1).toHex();
+    
+    if (admin.getPassword() == password)
+    {
+        msgBox.setWindowTitle("Info message");
+        msgBox.setText("Logged as: " + admin.getLogin());
+        msgBox.exec();
 
-    ui.groupBox_Login->setVisible(false);
+        // groupBox_main
+        ui.label_SignedInAs->setText("Signed in as:" + admin.getLogin());
+        ui.pushButton_SignInWindow->setVisible(false);
+        ui.pushButton_SignOut->setVisible(true);
+
+        // groupBox_SignIn
+        ui.groupBox_SignIn->setVisible(false);
+        
+        // groupBox_AdminStuff
+        ui.groupBox_AdminStuff->setVisible(true);
+    }
+    else
+    {
+        msgBox.setWindowTitle("Info message");
+        msgBox.setText("Incorrect password");
+        msgBox.exec();
+
+    }
+
+}
+
+// tu si pozriet, ako sa vytvaraju nove dialogove okienka a cez ne spravit pridavanie uzivatelov / zmenu ich udajov
+void Pharmacy::on_pushButton_AddCustomer_clicked()
+{
+    QString name = ui.lineEdit->text();
+    QString surname = ui.lineEdit_4->text();
+    QString adress = ui.lineEdit_5->text();
+    QString login = ui.lineEdit_6->text();
+    QString password = ui.lineEdit_7->text();
+    password = QCryptographicHash::hash(password.toStdString().c_str(), QCryptographicHash::Sha1).toHex();
+
+    customers.push_back(Customer(name, surname, adress, login, password));
+
+    ui.comboBox_users->addItem(login);
+}
+
+void Pharmacy::on_pushButton_AddPremiumCustomer_clicked()
+{
+    QString name = ui.lineEdit_2->text();
+    QString surname = ui.lineEdit_8->text();
+    QString adress = ui.lineEdit_9->text();
+    QString login = ui.lineEdit_10->text();
+    QString password = ui.lineEdit_11->text();
+    password = QCryptographicHash::hash(password.toStdString().c_str(), QCryptographicHash::Sha1).toHex();
+    int discount = ui.spinBox->value();
+
+    premiumCustomers.push_back(PremiumCustomer(name, surname, adress, discount, login, password));
+
+    ui.comboBox_users->addItem(login);
+}
+
+void Pharmacy::on_pushButton_AddEmployee_clicked()
+{
+    QString position = ui.lineEdit_3->text();
+    QString login = ui.lineEdit_12->text();
+    QString password = ui.lineEdit_13->text();
+    password = QCryptographicHash::hash(password.toStdString().c_str(), QCryptographicHash::Sha1).toHex();
+
+    employees.push_back(Employee(login, password, position));
+
+    ui.comboBox_users->addItem(login);
 }
--- Pharmacy.h
+++ Pharmacy.h
@@ -25,9 +25,24 @@ private:
 
     Admin admin;
 
+    bool loadUsers();
+    bool saveUsers();
+
 private slots:
 
+    void on_actionsave_users_triggered();
+
+    // groupBox_Main
     void on_pushButton_SignInWindow_clicked();
+    void on_pushButton_SignOut_clicked();
+    void on_pushButton_PrintUsers_clicked();
+    
+    // groupBox_Login
     void on_pushButton_SignInConfirm_clicked();
+
+    // groupBox_AdminStuff
+    void on_pushButton_AddCustomer_clicked();
+    void on_pushButton_AddPremiumCustomer_clicked();
+    void on_pushButton_AddEmployee_clicked();
 
 };
