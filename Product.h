32975056bc1b6fcba39fb89d2dd32049859cf48f
Tomas Homola <homola.tom6@gmail.com>
2020-12-24 22:13:43 +0100

    Mensie upravy

--- Product.h
+++ Product.h
@@ -7,21 +7,21 @@ class Product
 private:
 	int ID = -1;
 	QString productName = "";
-	int quantity = -1;
 	double price = -1.0;
+	int quantity = -1;
 
 public:
 	Product() {};
-	Product(int ID, QString productName, int quantity, double price) { this->ID = ID; this->productName = productName; this->quantity = quantity; this->price = price; }
+	Product(int ID, QString productName, double price, int quantity) { this->ID = ID; this->productName = productName; this->price = price; this->quantity = quantity; }
 
 	int getID() { return ID; }
 	QString getProductName() { return productName; }
-	int getQuantity() { return quantity; }
 	double getPrice() { return price; }
+	int getQuantity() { return quantity; }
 
 	void setID(int newID) { ID = newID; }
 	void setProductName(QString newProductName) { productName = newProductName; }
-	void setQuantity(int newQuantity) { quantity = newQuantity; }
 	void setPrice(double newPrice) { price = newPrice; }
+	void setQuantity(int newQuantity) { quantity = newQuantity; }
 
 };
