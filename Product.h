422e33faaf45123da82882451c75e67eb6b9ee61
Tomas Homola <homola.tom6@gmail.com>
2020-12-27 23:50:08 +0100

    Pridany QString pre popis produktu, pod tableWidget este oridat nejaky textEdit alebo nieco podobne, kde sa po kliknuti na nejaky produkt ukaze popis toho produktu

--- Product.h
+++ Product.h
@@ -5,26 +5,29 @@ class Product
 private:
 	int ID = -1;
 	QString productName = "";
+	QString productDescription = "";
 	double price = -1.0;
 	int quantity = -1;
 
 public:
 	Product() {};
-	Product(int ID, QString productName, double price, int quantity) { this->ID = ID; this->productName = productName; this->price = price; this->quantity = quantity; }
+	Product(int ID, QString productName, QString productDescription, double price, int quantity) { this->ID = ID; this->productName = productName; this->productDescription = productDescription; this->price = price; this->quantity = quantity; }
 
 	int getID() { return ID; }
 	QString getProductName() { return productName; }
+	QString getProductsDescription() { return productDescription; }
 	double getPrice() { return price; }
 	int getQuantity() { return quantity; }
 
 	void setID(int newID) { ID = newID; }
 	void setProductName(QString newProductName) { productName = newProductName; }
+	void setProductDescription(QString newProductDescription) { productDescription = newProductDescription; }
 	void setPrice(double newPrice) { price = newPrice; }
 	void setQuantity(int newQuantity) { quantity = newQuantity; }
 
 	void info()
 	{
-		qDebug() << "ID:" << ID << "Product name:" << productName << "Price:" << price << "Quantity:" << quantity << "\n";
+		qDebug() << "ID:" << ID << "Product name:" << productName << "Description:" << productDescription << "Price:" << price << "Quantity:" << quantity << "\n";
 	}
 
 };
